eaf698fb6368d8e354c277c95ecb30249b0fa511
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-12-09 22:21:05 +0300

    LINQ to none generics

--- LinqOverCollections/Program.cs
+++ LinqOverCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using LinqOverCollections;
+using System.Collections;
 
 Console.WriteLine("***** LINQ over Generic Collections *****\n");
 
@@ -13,7 +14,9 @@ List<Car> myCars = new List<Car>()
  };
 
 //GetFastCars(myCars);
-GetFastBMWs(myCars);
+//GetFastBMWs(myCars);
+//LINQOverArrayList();
+OfTypeAsFilter();
 
 Console.ReadLine();
 
@@ -35,5 +38,42 @@ static void GetFastBMWs(List<Car> myCars)
 	foreach (var car in fastCars)
 	{
 		Console.WriteLine("{0} is going too fast!", car.PetName);
+	}
+}
+
+static void LINQOverArrayList()
+{
+	Console.WriteLine("***** LINQ over ArrayList *****");
+
+	// Here is a nongeneric collection of cars.
+	ArrayList myCars = new ArrayList()
+	{
+		new Car{ PetName = "Henry", Color = "Silver", Speed = 100, Make = "BMW"},
+		new Car{ PetName = "Daisy", Color = "Tan", Speed = 90, Make = "BMW"},
+		new Car{ PetName = "Mary", Color = "Black", Speed = 55, Make = "VW"},
+		new Car{ PetName = "Clunker", Color = "Rust", Speed = 5, Make = "Yugo"},
+		new Car{ PetName = "Melvin", Color = "White", Speed = 43, Make = "Ford"}
+	};
+	// Transform ArrayList into an IEnumerable<T>-compatible type.
+	var myCarsEnum = myCars.OfType<Car>();
+
+	// Create a query expression targeting the compatible type.
+	var fastCars = from c in myCarsEnum where c.Speed > 55 select c;
+	foreach (var car in fastCars)
+	{
+		Console.WriteLine("{0} is going too fast!", car.PetName);
+	}
+}
+
+static void OfTypeAsFilter()
+{
+	// Extract the ints from the ArrayList.
+	ArrayList myStuff = new ArrayList();
+	myStuff.AddRange(new object[] { 10, 400, 8, false, new Car(), "string data" });
+	var myInts = myStuff.OfType<int>();
+	// Prints out 10, 400, and 8.
+	foreach (int i in myInts)
+	{
+		Console.WriteLine("Int value: {0}", i);
 	}
 }
