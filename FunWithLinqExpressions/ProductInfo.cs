a77bec9de8ae32a2fc50670605837afc6510669e
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-12-10 23:42:58 +0300

    Fun with LINQ  operations

---
+++ FunWithLinqExpressions/ProductInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace FunWithLinqExpressions
+{
+	class ProductInfo
+	{
+		public string Name { get; set; } = "";
+		public string Description { get; set; } = "";
+		public int NumberInStock { get; set; } = 0;
+
+		public override string ToString()
+			=> $"Name = {Name}, Description = {Description}, Number in Stock = {NumberInStock}";
+	}
+}
